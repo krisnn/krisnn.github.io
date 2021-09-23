51734c37b7e3cb458e45dbea9b1ea81a79ea47fb
norman <norman.kristiono@gmail.com>
2021-09-23 11:42:37 +0700

    add sub directory folder backup

---
+++ backup/normankristiono.net/WEB/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0">
+		
+		<title>Welcome to Krisn Website</title>
+		<meta name="description" content="Web developer and it engineering focus on learning to be devops and also enjoy learning everything."/>
+		<meta name="keywords" content="norman kristiono, normankristiono.net, krisn, krisnn_, 404website" />
+		
+		<meta name="apple-mobile-web-app-title" content="NXNK">
+		<meta name="author" content="Norman Kristiono" />
+		<meta http-equiv="cleartype" content="on">
+
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/images/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/images/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/images/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/images/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/images/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/images/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="assets/images/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
+		<link rel="manifest" href="assets/images/manifest.json">
+		<meta name="msapplication-TileColor" content="#fff">
+		<meta name="msapplication-TileImage" content="assets/images/ms-icon-144x144.png">
+		<meta name="theme-color" content="#fff">
+
+		<link rel="stylesheet" href="assets/style.css" type="text/css" media="screen" />
+		<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Arimo&subset=latin' />
+		
+	</head>
+  <body>
+
+    <header>
+      <!--<img src="assets/images/logo.png" class="logo">-->
+      <H1>
+		Norman <i>Kris</i>tiono<br>
+		<i>IT Enthusiast</i>.
+	  </H1>
+    </header>
+	
+	<body>
+		<section class="intro">
+		  <hr>
+		  <p>"Writing enlarges the landscape of the mind. " ― V.S. Pritchett</p>
+		  <hr>
+		  <ul>
+			<li><a href="http://blog.normankristiono.net/" title="Blog">Blog</a></li>
+			<li><a href="http://normankristiono.net/about" title="Blog">About</a></li>
+			<li><a href="https://www.linkedin.com/in/norman-kristiono-27aa8278/" title="Linkedin Profile">LinkedIn</a></li>
+			<li><span class="emailme" title="Email me!"></span></li>
+		  </ul>
+		</section>
+	</body>
+
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+
+  </body>
+</html>
